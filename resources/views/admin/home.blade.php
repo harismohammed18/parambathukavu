90da8778a09ef360043d0937d90068c926c6b416
harismohammed <harismohammed18@gmail.com>
2018-04-03 10:16:44 +0530

    admin panel completed

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,58 +1,5 @@
 @extends('admin.layout.app')
 
 @section('content')
-  <div class="row">
-    <div class="col-md-3">
-      <div class="card bg-primary p-20">
-        <div class="media widget-ten">
-          <div class="media-left meida media-middle">
-            <span><i class="fa fa-usd f-s-40"></i></span>
-          </div>
-          <div class="media-body media-text-right">
-            <h2 class="color-white">278</h2>
-            <p class="m-b-0">Total Revenue</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-3">
-      <div class="card bg-pink p-20">
-        <div class="media widget-ten">
-          <div class="media-left meida media-middle">
-            <span><i class="fa fa-shopping-cart f-s-40"></i></span>
-          </div>
-          <div class="media-body media-text-right">
-            <h2 class="color-white">278</h2>
-            <p class="m-b-0">Sales</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-3">
-      <div class="card bg-danger p-20">
-        <div class="media widget-ten">
-          <div class="media-left meida media-middle">
-             <span><i class="fa fa-home f-s-40"></i></span>
-          </div>
-          <div class="media-body media-text-right">
-            <h2 class="color-white">278</h2>
-            <p class="m-b-0">Host</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-3">
-      <div class="card bg-success p-20">
-        <div class="media widget-ten">
-          <div class="media-left meida media-middle">
-            <span><i class="fa fa-user f-s-40"></i></span>
-          </div>
-          <div class="media-body media-text-right">
-            <h2 class="color-white">278</h2>
-            <p class="m-b-0">Users</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  
 @endsection
--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -5,12 +5,6 @@
         <ul id="sidebarnav">
             <li class="nav-devider"></li>
             <li class="nav-label">Home</li>
-            <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-tachometer"></i><span class="hide-menu">Dashboard <span class="label label-rouded label-primary pull-right">2</span></span></a>
-                <ul aria-expanded="false" class="collapse">
-                    <li><a href="index.html">Ecommerce </a></li>
-                    <li><a href="index1.html">Analytics </a></li>
-                </ul>
-            </li>
             <li @if(isset($menu) && $menu == '1') class="active" @endif> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-image"></i><span class="hide-menu">Gallery</a>
                 <ul aria-expanded="false" class="collapse">
                     <li><a href="{{ route('gallery.add') }}"><i class="fa fa-plus text-success"></i> Add </a></li>
